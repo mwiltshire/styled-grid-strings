ca15615d37c30e7ed031789ef640116e89b8155c
Mark Wiltshire <mark.wiltshire88@gmail.com>
2020-09-12 21:00:59 +0200

    typo

--- src/create-grid.ts
+++ src/create-grid.ts
@@ -6,7 +6,7 @@ import { GridOptions, GridHelpers } from './types';
  * Configure grid breakpoints, direction and spacing options.
  *
  * This function returns an object containing two helpder functions,
- * `row` and `column` configured to use the options you passed.
+ * `row` and `column`, configured to use the options you passed.
  *
  * `column` can take further options, `sizes` and `offsets` to
  * configure properties for a specific grid column.
